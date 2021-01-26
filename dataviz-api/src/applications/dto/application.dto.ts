0a8ca67a4b5e42f191992746f1fc869e8bf6c3a4
NmiDev <missiaen.nicolas@gmail.com>
2021-01-26 19:11:36 +0100

    update application entity and dto

--- dataviz-api/src/applications/dto/application.dto.ts
+++ dataviz-api/src/applications/dto/application.dto.ts
@@ -1,4 +1,4 @@
-import { IsString } from 'class-validator';
+import { IsDateString, IsNumber, IsString } from 'class-validator';
 
 export class ApplicationDto {
   @IsString()
@@ -7,6 +7,27 @@ export class ApplicationDto {
   @IsString()
   readonly name: string;
 
+  @IsString()
+  readonly version: string;
+
+  @IsDateString()
+  readonly createdAt: Date;
+
+  @IsString()
+  readonly status: string;
+
   @IsString({ each: true })
   readonly components: string[];
+
+  @IsNumber()
+  readonly businessValue: number;
+
+  @IsNumber()
+  readonly technicalHealth: number;
+
+  @IsNumber()
+  readonly complexity: number;
+
+  @IsString()
+  readonly criticality: string;
 }
--- dataviz-api/src/applications/entities/application.entity.ts
+++ dataviz-api/src/applications/entities/application.entity.ts
@@ -9,8 +9,29 @@ export class Application extends Document {
   @Prop()
   name: string;
 
+  @Prop()
+  version: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  status: string;
+
   @Prop([String])
   components: string[];
+
+  @Prop()
+  businessValue: number;
+
+  @Prop()
+  technicalHealth: number;
+
+  @Prop()
+  complexity: number;
+
+  @Prop()
+  criticality: string;
 }
 
 export const ApplicationSchema = SchemaFactory.createForClass(Application);
